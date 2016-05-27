0b9459d03fdb7fd20fefd04d4e921588a0fd5c4a
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-26 23:09:21 -0400

    Initial Commit...set up all var  and public method

---
+++ COMP123_300845885_A1/Hero.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_300845885_A1
+{ /*
+    *Author: Krytia Fernandez
+    *Version: 0.0.0.1 == Initial Commit...set up all var  and public method 
+    */
+    class Hero
+    {
+        //+++++++++++++++++++++Private var++++++++++++++++++
+        private int _strength;
+        private int _speed;
+        private int _health;
+        private string _name;
+        //+++++++++++++++++++++Public Var++++++++++++++++++
+        public string Name
+        {
+            get
+
+            {
+
+                return this._name;
+
+            }
+
+            set
+
+            {
+
+                this._name = value;
+            }
+        }
+        private void _generateAbilities()
+        {
+            {
+
+            }
+        }
+        public void Fight ()
+        {
+
+        }
+        public void Show()
+        {
+
+        }
+        private bool _hitAttempt()
+        {
+
+        }
+
+        private int _hitDamage()
+        {
+
+        }
+
+        
+        //+++++++Private Method+++++++++
+        //+++++++Public Medod+++++++++
+    }
+}
