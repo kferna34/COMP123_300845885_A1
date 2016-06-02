a3cc9cd37716ec738dba7b8b9573faaa91d52ef1
kferna34 <kferna34@my.centennialcollege.ca>
2016-06-02 01:04:11 -0400

    The Hero class works fine now.... finalized Show method, fight method and made some "decorations" == works, but not done yet

--- COMP123_300845885_A1/Hero.cs
+++ COMP123_300845885_A1/Hero.cs
@@ -7,79 +7,176 @@ using System.Threading.Tasks;
 namespace COMP123_300845885_A1
 { /*
     *Author: Krytia Fernandez
-    *Version: 0.0.0. == did the _generate() to display speed, strength&health&& set the properties, also as the condition for the hitattempt ==not done 
+    *Version: 0.0.0.2 == The Hero class works fine now.... finalized Show method, fight method and made some "decorations" == works, but not done yet
     */
     class Hero
     {
-        //+++++++++++++++++++++Private var++++++++++++++++++
-        private int _strength { get; set; }
-        private int _speed { get; set; }
-        private int _health { get; set; }
-        private string _name;
-        public bool _hitAttempt { get; private set; }
-        public int _hitDamage { get; private set; }
-        //+++++++++++++++++++++Public Var++++++++++++++++++
+
+        //*******************************PRIVATE INSTANCE VAR*******************************************************
+
+        int _strength;
+
+        int _speed;
+
+        int _health;
+        string _name;
+
+        //******************************PUBLIC PROP//***************************************************************
         public string Name
+
         {
+
             get
 
             {
 
-                return this.Name;
+                return this._name;
 
             }
 
             set
 
             {
+                //Console.WriteLine("_name value set");
+                this._name = value;
 
-                this.Name = value;
             }
-        }
-
-
-
-        public Hero(string name)
-        {
-            this.Name = name;
-            this._generateAbilities();//call method  == nts:() means method
 
         }
 
-        private void _generateAbilities() //returns strength, speed,health
+        public bool hitAttempt
         {
-            Console.WriteLine(_strength);
-            Console.WriteLine(_speed);
-            Console.WriteLine(_health);
-        }
-
-        public void Fight()
-        {
-            if (_hitAttempt == true)
+            get
             {
-               
-                Console.WriteLine(_hitAttempt);
-                
+                return hitAttempt;
+            }
+            set
+            {
+                hitAttempt = value;
             }
         }
+
+        public int Strength
+        {
+            get
+            {
+                return _strength;
+            }
+
+            set
+            {
+                _strength = value;
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return _speed;
+            }
+
+            set
+            {
+                _speed = value;
+            }
+        }
+
+        public int Health
+        {
+            get
+            {
+                return _health;
+            }
+
+            set
+            {
+                _health = value;
+            }
+        }
+
+
+        public Hero(string Name)
+
+        {
+
+            this._name = Name;
+            this._generateAbilities(80, 95, 100);
+
+        }
+        /*
+        *<summary>
+        * this is a constuctor that takes strength, speed, health as param
+        *assigns the local speedval to the speed prop
+        * assigns the local strengthval to the strength prop
+        * assigns the local  healthval to the health prop
+        *</summary>
+        */
+        private void _generateAbilities(int strength, int speed, int health)
+
+        {
+            this.Strength = strength;
+            this.Speed = speed;
+            this.Health = health;
+
+
+        }
+        public void Fight(bool hitAttempt)
+        {
+
+            // for the condition
+            int hit = Strength * 5;
+            int health = this.Health - hit;
+
+            if (hitAttempt == true)
+            {
+                Console.WriteLine("****************You have been hit********************");
+                Console.WriteLine(" Damaged by {0},  Remaining Health {1} ", hit * 5, health);
+            }
+
+            else
+            {
+                Console.WriteLine(" The hit did not cause damage, showing overall abilities now");
+                Console.WriteLine("=============================================================");
+                Console.WriteLine("========================");
+                Console.WriteLine("Remaining health {0},Strength:{1},Speed{2} ", Health, this.Strength, this.Speed);
+                Console.WriteLine();
+            }
+
+        }
+        private void _hitAttempt(bool hitAttempt)
+        {
+            int hit = Strength * 5;
+            bool attempt = true;
+
+            if (attempt)
+            {
+                Console.WriteLine("****************You have been hit********************");
+                Console.WriteLine(" Damaged by {0},  Remaining Health {1} ", hit * 5, Health);
+            }
+            else
+            {
+                Console.WriteLine(" The hit did not cause damage, showing overall abilities now");
+                Console.WriteLine("Remaining health {0},Strength:{1},Speed{2} ", this.Health, this.Strength, this.Speed);
+
+            }
+        }
+        private void _hitDamage(int hitDamage)
+        {
+            int hit = Strength * 5;
+            Console.WriteLine(" Damaged by {0},  Remaining Health {1} ", hit * 5, Health - hit);
+
+        }
+        public void Show()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("                      " + this.Name);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("  strength:         " + this.Strength);
+            Console.WriteLine("  speed   :         " + this.Speed);
+            Console.WriteLine("  health  :         " + this.Health);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+        }
     }
-
-    public void Show()
-    {
-
-    }
-    private bool _hitAttempt()
-    {
-       
-    }
-
-    private int _hitDamage()
-    {
-
-    }
-
-
-    //+++++++Private Method+++++++++
-    //+++++++Public Medod+++++++++
-}
 }
